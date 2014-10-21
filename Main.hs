f71d8f88d0ba656b3a62ec7fa52d193e692bd03b
Etienne Chartrand <chartrand.etienne@gmail.com>
2014-10-20 23:35:10 -0400

    I actually put my DB in a ReaderT which is pretty cool

--- Main.hs
+++ Main.hs
@@ -5,6 +5,10 @@ import Control.Monad.Trans (liftIO)
 import Database.SQLite
 import Data.MarkovChain
 import System.Random
+import Control.Monad.Reader
+
+-- type DbMonad a = ReaderT SQLiteHandle ServerPart a
+type DbMonad = ReaderT SQLiteHandle (ServerPartT IO)
 
 -- STX and ETX stand for start and end of text. I want the algo to know where a phrase began. 
 -- This is gonna be the extent of it while I use Data.MarkovChain, 
@@ -21,31 +25,33 @@ magic :: Int -> String -> String
 magic len input = take len $ run 2 input 0 (mkStdGen 123)
 
 
-handleText :: String -> String -> IO String
-handleText t w = do
+handleText :: SQLiteHandle -> String -> String -> IO String
+handleText handle t w = do
     let entryLength = length t
-    handle <- openConnection "db.sql"
+    -- handle <- openConnection "db.sql"
     insert <- execParamStatement_ handle "INSERT INTO entries (what, who) VALUES (:to_insert, :who)" [(":to_insert", Text t), (":who", Text w)]  
     entries <- execParamStatement handle "SELECT what FROM entries WHERE who = :who" [(":who", Text w)] :: IO (Either String [[Row String]])
     case entries of 
         Left err -> return err
         Right [rows] -> return $ magic entryLength $ extractFromDBOut rows
 
--- execParamStatement_ :: SQLiteHandle -> String -> [(String, Value)] -> IO (Maybe String)
--- execStatement :: SQLiteResult a => SQLiteHandle -> String -> IO (Either String [[Row a]])
 
 myPolicy :: BodyPolicy
 myPolicy = (defaultBodyPolicy "/tmp/" 0 1000 1000)
 
-serverText :: ServerPart String
+serverText :: DbMonad String
 serverText = do
     decodeBody myPolicy
     input <- look "input"
     who <- look "who"
-    handled <- liftIO $ handleText input who
+    handle <- ask
+    handled <- liftIO $ handleText handle input who
     ok $ handled 
 
+runDbMonad :: DbMonad a -> ServerPart a
+runDbMonad m = do
+    db <- liftIO $ openConnection "db.sql"
+    runReaderT m db
+
 main :: IO ()
-main = simpleHTTP nullConf $ serverText
-
-
+main = simpleHTTP nullConf $ runDbMonad serverText 
